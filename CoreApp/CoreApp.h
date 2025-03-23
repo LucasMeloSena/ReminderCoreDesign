abafaf908a118bd629f80676b60cd93237e93c7d
Lucas Sena <lucasmelosena@gmail.com>
2025-03-23 17:39:20 -0300

    feat: setup Reminder App Core Design project

---
+++ CoreApp/CoreApp.h
@@ -0,0 +1,18 @@
+//
+//  CoreApp.h
+//  CoreApp
+//
+//  Created by Lucas Sena on 23/03/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CoreApp.
+FOUNDATION_EXPORT double CoreAppVersionNumber;
+
+//! Project version string for CoreApp.
+FOUNDATION_EXPORT const unsigned char CoreAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreApp/PublicHeader.h>
+
+
---
+++ CoreApp/Sources/Constants/Metrics/Metrics-Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Metrics-Constants.swift
+//  CoreApp
+//
+//  Created by Lucas Sena on 23/03/25.
+//
+
+import Foundation
+import UIKit
+
+public struct Metrics {
+    static let huge: CGFloat = 48
+    static let high: CGFloat = 32
+    static let medium: CGFloat = 24
+    static let medier: CGFloat = 16
+    static let small: CGFloat = 12
+    static let tiny: CGFloat = 8
+    static let little: CGFloat = 4
+    static let buttonSize: CGFloat = 56
+    static let inputSize: CGFloat = 56
+    static let profileImageSize: CGFloat = 68
+    static let backgroundProfileSize: CGFloat = 300
+    static let profileImageTopDistance: CGFloat = 100
+}
