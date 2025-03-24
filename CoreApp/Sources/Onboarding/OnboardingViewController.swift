5541b35f118e80189d8d1c10daeae2c61047e514
Lucas Sena <lucasmelosena@gmail.com>
2025-03-24 19:01:46 -0300

    refactor: transform view in view controller

--- CoreApp/Sources/Onboarding/OnboardingViewController.swift
+++ CoreApp/Sources/Onboarding/OnboardingViewController.swift
@@ -9,20 +9,22 @@ import Foundation
 import UIKit
 
 public class OnboardingViewController: UIViewController {
-    private var steps: [String] = []
-    private var currentStep = 0
+    private var content: (title: String, description: [String])?
     
-    private let backgroundView: UIView = {
-        let view = UIView()
-        view.backgroundColor = Colors.gray100.withAlphaComponent(0.4)
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.font = Typography.heading
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
-    private let messageLabel: UILabel = {
+    private let descriptionLabel: UILabel = {
         let label = UILabel()
-        label.textColor = Colors.gray300
-        label.font = Typography.heading
+        label.textColor = .black
+        label.font = Typography.subHeading
         label.numberOfLines = 0
         label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -31,32 +33,37 @@ public class OnboardingViewController: UIViewController {
     
     private let nextButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Próximo", for: .normal)
-        button.setTitleColor(Colors.primaryRedBase, for: .normal)
-        button.backgroundColor = .white
-        button.titleLabel?.font = Typography.subHeading
+        button.setTitle("Continuar", for: .normal)
+        button.backgroundColor = Colors.primaryRedBase
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = Typography.label
         button.addTarget(self, action: #selector(didTappedNextStep), for: .touchUpInside)
+        button.layer.cornerRadius = Metrics.small
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
-     
-     public init() {
-         super.init(nibName: nil, bundle: nil)
-     }
-     
-     required init?(coder: NSCoder) {
-         fatalError("init(coder:) has not been implemented")
-     }
-     
+    
+    public init(content: (title: String, description: [String])) {
+        self.titleLabel.text = content.title
+        self.descriptionLabel.text = content.description.joined(separator: "\n\n")
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         setupUI()
     }
     
     private func setupUI() {
+        view.backgroundColor = .white
         view.translatesAutoresizingMaskIntoConstraints = false
         
-        view.addSubview(backgroundView)
-        view.addSubview(messageLabel)
+        view.addSubview(titleLabel)
+        view.addSubview(descriptionLabel)
         view.addSubview(nextButton)
         
         setupConstraints()
@@ -64,41 +71,29 @@ public class OnboardingViewController: UIViewController {
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-            backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
-            backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: Metrics.huge),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.medium),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.medium),
             
-            messageLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            messageLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            messageLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.medium),
-            messageLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.medium),
+            descriptionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            descriptionLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.medium),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.medium),
             
-            nextButton.topAnchor.constraint(equalTo: messageLabel.bottomAnchor, constant: Metrics.medium),
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            nextButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -Metrics.medium),
+            nextButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Metrics.huge),
+            nextButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Metrics.huge),
+            nextButton.heightAnchor.constraint(equalToConstant: 40)
         ])
     }
     
-    public func presentOnboarding(on view: UIView, with steps: [String]) {
-        self.steps = steps
-        self.currentStep = 0
-    }
-    
-    private func updateStep() {
-        messageLabel.text = steps[currentStep]
-    }
-    
     private func dismiss() {
-        //removeFromSuperview()
+        dismiss(animated: true)
     }
     
     @objc
     private func didTappedNextStep() {
-        currentStep += 1
-        if (currentStep < steps.count) {
-            updateStep()
-        } else {
-            dismiss()
-        }
+        dismiss()
     }
 }
